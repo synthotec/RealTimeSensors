c4c87c5cf7f83b84605cc7a17411d74d2db6332d
James Prince <jprince@synthotec.com>
2022-10-26 11:36:01 +0100

    Added output in zone format +  zone setup/arrays

--- RealTimeSensors.ino
+++ RealTimeSensors.ino
@@ -1,53 +1,76 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define VERSION 2
-#define ONE_WIRE_BUS 4
+#define Version 7
 
+//######### ZONE SETUP ##########
+#define PinCount 1
+int PinNumbers[] = {D2}; // Add multiple entries in this format: {D1, D2, D3}
+int ZoneNumbers[] = {0};	 // Add multiple entries in this format: {0, 1, 2}
+//########## ZONE SETUP ##########
 
-
-
-OneWire OneWire(ONE_WIRE_BUS);
-DallasTemperature TemperatureSensors(&OneWire);
-
-int LED = 2;
-
+OneWire OneWires[PinCount];
+DallasTemperature DallasTemperatures[PinCount];
 
 void setup(void)
 {
-  Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT);
+	for (int PinIndex = 0; PinIndex <= (PinCount - 1); PinIndex++)
+	{
+		OneWires[PinIndex] = OneWire(PinNumbers[PinIndex]);
+		DallasTemperatures[PinIndex] = DallasTemperature(&OneWires[PinIndex]);
+		DallasTemperatures[PinIndex].begin();
+	}
+
+	pinMode(LED_BUILTIN, OUTPUT);
+	Serial.begin(115200);
 }
 
-void loop(void){
-  TemperatureSensors.requestTemperatures();
-String outputString = "{\"Version\":";
-outputString+=VERSION;
+void loop(void)
+{
 
-outputString+=", \"TemperatureSensors\":[";
-TemperatureSensors.begin();
-  for (int index = 0; index < TemperatureSensors.getDeviceCount(); index++)
-    {
-    float tempC = TemperatureSensors.getTempCByIndex(index);
+	String JsonString = "{\"TemperatureZones\":[";
 
-      if(index>0)
-        {
-        outputString+=",";
-        }
-        
-      outputString+="{\"Index\":";
-      outputString+=index;
-      outputString+=", \"CelciusValue\":";
-      outputString+=tempC;
-      outputString+="}";      
-    }
-  outputString+="]}";
-  Serial.println(outputString);
-  
-  digitalWrite(LED_BUILTIN, LOW); 
-  delay(500);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
+	for (int PinIndex = 0; PinIndex <= (PinCount - 1); PinIndex++)
+	{
 
+		if (PinIndex > 0)
+		{
+			JsonString += ",";
+		}
 
+		JsonString += "{\"Zone\":";
+		JsonString += ZoneNumbers[PinIndex];
+		JsonString += ", \"Sensors\":[";
+
+		DallasTemperatures[PinIndex].begin();
+		DallasTemperatures[PinIndex].requestTemperatures();
+
+		for (uint8_t SensorIndex = 0; SensorIndex < DallasTemperatures[PinIndex].getDeviceCount(); SensorIndex++)
+		{
+
+			if (SensorIndex > 0)
+			{
+				JsonString += ",";
+			}
+
+			JsonString += "{\"Index\":";
+			JsonString += SensorIndex;
+
+			JsonString += ", \"CelciusValue\":";
+			JsonString += DallasTemperatures[PinIndex].getTempCByIndex(SensorIndex);
+			JsonString += "}";
+		}
+		JsonString += "]}";
+	}
+
+	JsonString += "]";
+	JsonString += ",\"Version\":";
+	JsonString += Version;
+	JsonString += "}";
+	Serial.println(JsonString);
+
+	digitalWrite(LED_BUILTIN, LOW);
+	delay(1000);
+	digitalWrite(LED_BUILTIN, HIGH);
+	delay(1000);
 }
