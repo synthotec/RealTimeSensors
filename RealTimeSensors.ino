24611b958bfc037ee228f409b7416ca3244b3116
James Prince <jprince@synthotec.com>
2023-01-23 12:41:55 +0000

    v15 - Added Zone Names to JSON

--- RealTimeSensors.ino
+++ RealTimeSensors.ino
@@ -1,12 +1,13 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define Version 14
+#define Version 15
 
 //######### ZONE SETUP ##########
 #define PinCount 5
 int PinNumbers[] = { D2, D3, D4, D5, D6 }; // Add multiple entries in this format: {D1, D2, D3}
 int ZoneNumbers[] = { 0, 1, 2, 3, 4 }; // Add multiple entries in this format: {0, 1, 2}
+string ZoneNames[] = {"No Zones","Water In","Water Out","Fixed Half","Moving Half"}
 // Z0 - No Zones / Water In + Water Out %DEPRECATED%
 // Z1 = Water In
 // Z2 = Water Out
@@ -61,6 +62,11 @@ void loop(void)
 
 			JsonString += ", \"Zone\":";
 			JsonString += ZoneNumbers[PinIndex];
+
+			JsonString += ", \"ZoneName\":";
+			JsonString +="\"";
+			JsonString +=ZoneNames[PinIndex];
+			JsonString +="\"";
 
 			float TempC = DallasTemperatures[PinIndex].getTempCByIndex(SensorIndex);
 
